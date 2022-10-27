cec1bae4c5e9f33eca10c5af959ce88952ff85d5
Loren <lorensprague@icloud.com>
2022-10-27 16:05:55 -0400

    completed assignment

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,19 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all 
+        render json: students
+    end
+
+    def grade
+        # student_order = Student.order(grade: :desc)
+        student_order = Student.all.order(grade: :desc)
+        render json: student_order
+    end
+
+    def highest_grade
+        highest = Student.order(grade: :desc).first
+        render json: highest
+    end
+
+end
